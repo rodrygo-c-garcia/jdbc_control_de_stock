34f1b4540c0a0f4af34811923b00bbf550ed7524
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-08-28 12:00:14 -0400

    feat: add try cath in connection jdbc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,17 @@ package org.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/control_stock?useTimezone=true&serverTimezone=UTC";
         String user = "root";
-        Connection connection = DriverManager.getConnection(url, user, null);
+        // siempre tratar las conexiones dentro de un try catch
+        try {
+            Connection connection = DriverManager.getConnection(url, user, null);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
