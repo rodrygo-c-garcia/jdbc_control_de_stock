6f0488b86a2fc601724d1151dc7879ac92795834
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-08-28 11:58:14 -0400

    feat: add connection JDBC

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String url = "jdbc:mysql://localhost:3306/control_stock?useTimezone=true&serverTimezone=UTC";
+        String user = "root";
+        Connection connection = DriverManager.getConnection(url, user, null);
     }
 }
