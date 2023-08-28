b3bbfc73a8f9e47eaa294a90617f5d25f7fc6124
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-08-28 12:17:11 -0400

    feat: test connection to jdbc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         String url = "jdbc:mysql://localhost:3306/control_stock?useTimezone=true&serverTimezone=UTC";
         String user = "root";
         // siempre tratar las conexiones dentro de un try catch
         try {
             Connection connection = DriverManager.getConnection(url, user, null);
+            System.out.println("Conexión exitosa");
+            connection.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
