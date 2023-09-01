9fc11a66e098d1a84d24c7ff311ac63ffcea9a18
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-09-01 10:23:05 -0400

    feat: add statement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args)  {
@@ -11,7 +9,18 @@ public class Main {
         // siempre tratar las conexiones dentro de un try catch
         try {
             Connection connection = DriverManager.getConnection(url, user, null);
-            System.out.println("Conexión exitosa");
+            // Statement es una interfaz que se utiliza para ejecutar instrucciones SQL en una base de datos
+            Statement statement = connection.createStatement();
+            // execute() devuelve un booleano, true si es un ResultSet, false si no lo es
+            boolean result = statement.execute("SELECT * FROM producto");
+            System.out.println(result);
+
+            // ResultSet es una interfaz que se utiliza para representar el conjunto de resultados de una base de datos
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM producto");
+            // mientras tengamos datos para leer, vamos imprimiendo
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("nombre"));
+            }
             connection.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
